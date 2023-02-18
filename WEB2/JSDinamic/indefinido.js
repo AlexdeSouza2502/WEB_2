66ffbf63f8e5e4033766fb29c1b5d65f5ce208b1
Alex_Souza2002 <62069087+AlexdeSouza2502@users.noreply.github.com>
2023-02-17 22:17:56 -0300

    Add files via upload

---
+++ WEB2/JSDinamic/indefinido.js
@@ -0,0 +1,7 @@
+let minhaVar = null;
+
+console.log(minhaVar);
+
+const x = 10;
+const soma = x + minhaVar;
+console.log(soma,typeof soma);
---
+++ WEB2/aula1/teste aula 1.js
@@ -0,0 +1,7 @@
+const idade = 40;
+console.log(typeof idade);
+// string
+console.log(idade.toFixed(2))
+//
+console.log(typeof idade);
+//number
